51e69c0785995523c922407581d9c6ba75c20f6b
daniel <dbarretobrito@gmail.com>
2024-02-28 17:43:05 -0300

    #

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
-const express = require('express');
+// Importando bibliotecas
+const express = require("express");
+
+// Importando rotas (ele vai buscar o arquivo index.js)
+const routes = require('./routes');
+
+// Inicializando biblioteca
 const app = express();
+
+// Atribuindo o tipo de dado que será enviado pelo body e as rotas que serão utilizadas
+app.use(express.json());
+app.use(routes)
 
 const PORT = 3333;
 app.listen(PORT, () => {
-  console.log(`O servidor local com node está rodando na porta: ${PORT}`);
+    console.log(`O servidor local com node está rodando na porta: ${PORT}`)
 });
