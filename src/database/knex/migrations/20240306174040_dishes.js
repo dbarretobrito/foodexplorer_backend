1e83065736c37f7a3663f54b1f19104fe0f0c465
daniel <dbarretobrito@gmail.com>
2024-03-07 14:42:06 -0300

    Implementa a criação de tabelas via migrate

--- src/database/knex/migrations/20240306174040_dishes.js
+++ src/database/knex/migrations/20240306174040_dishes.js
@@ -1,3 +1,14 @@
-exports.up = knex => knex.schema.createTable('dishes', table => {});
+exports.up = knex =>
+  knex.schema.createTable('dishes', table => {
+    table.increments('id');
+    table.text('title').notNullable();
+    table.text('description').notNullable();
+    table.text('category').notNullable();
+    table.text('image').notNullable();
+    table.decimal('price', 14, 2).notNullable();
+
+    table.timestamp('created_at').default(knex.fn.now());
+    table.timestamp('updated_at').default(knex.fn.now());
+  });
 
 exports.down = knex => knex.schema.dropTable('dishes');
