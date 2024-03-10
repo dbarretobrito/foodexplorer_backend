b0bd34dbd5d2a7336d4e170b4748f5ada4cfe13f
daniel <dbarretobrito@gmail.com>
2024-03-10 18:47:50 -0300

    Cria o método show (get) para a tabela de dishes

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -25,6 +25,25 @@ class DishesController {
 
     return response.status(201).json();
   }
+
+  async show(request, response) {
+    // Pegando o id do prato que é passado como parâmetro
+    const { id } = request.params;
+
+    // Fazendo a busca na tabela dishes e pedindo para buscar somente o dado que tenha o ID que foi passado como parâmetro.
+    const dish = await knex('dishes').where({ id }).first();
+
+    // Fazendo a busca dos ingredients também. Passando dish_id que é o parâmetro passado pela request e ordenado por ordem alfabética
+    const ingredients = await knex('ingredients')
+      .where({ dish_id: id })
+      .orderBy('title');
+
+    // Fazendo o retorno
+    return response.status(200).json({
+      ...dish,
+      ingredients,
+    });
+  }
 }
 
 module.exports = DishesController;
