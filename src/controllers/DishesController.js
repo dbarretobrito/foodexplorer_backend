eff14d091fd73e0675edd99419c5133dfc24d098
daniel <dbarretobrito@gmail.com>
2024-03-13 09:44:11 -0300

    Cria rotas e controller para criação de pratos do foodexplorer

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -2,27 +2,32 @@ const knex = require('../database/knex');
 
 class DishesController {
   async create(request, response) {
-    // Parâmetros enviados pelo body
+    // ... existing code for getting other dish data (description, category, etc.)
+
     const { title, description, category, image, price, ingredients } =
       request.body;
 
-    // Inserindo o prato e todos os seus dados
-    const dish_id = await knex('dishes').insert({
+    // Insert the new dish in the "dishes" table
+    const insertedDish = await knex('dishes').insert({
       title,
       description,
       category,
       price,
     });
 
-    // Inserindo os ingredients passado no dish na tabela ingredients
-    const ingredientsInsert = ingredients.map(ingredient => {
-      return {
-        title: ingredient,
-        dish_id,
-      };
-    });
+    // Get the ID of the inserted dish (assuming single row insertion)
+    const dish_id = insertedDish[0];
+
+    // Map ingredients to an array of objects for insertion
+    const ingredientsInsert = ingredients.map(ingredient => ({
+      title: ingredient,
+      dish_id,
+    }));
+
+    // Insert the ingredients into the "ingredients" table
     await knex('ingredients').insert(ingredientsInsert);
 
+    // Return a success response
     return response.status(201).json();
   }
 }
