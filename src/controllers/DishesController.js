0f3f910a1104e6df33c893614c2b009e771117b4
daniel <dbarretobrito@gmail.com>
2024-03-13 14:55:16 -0300

    Cria o método show (get) para a tabela de dishes

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -2,23 +2,23 @@ const knex = require('../database/knex');
 
 class DishesController {
   async create(request, response) {
-    // ... existing code for getting other dish data (description, category, etc.)
-
+    // Parâmetros enviados pelo body
     const { title, description, category, image, price, ingredients } =
       request.body;
 
-    // Insert the new dish in the "dishes" table
+    // Inserindo o prato e todos os seus dados na tabela dishes
     const insertedDish = await knex('dishes').insert({
       title,
       description,
       category,
+      image,
       price,
     });
 
-    // Get the ID of the inserted dish (assuming single row insertion)
+    // Obtendo o ID do prato inserido
     const dish_id = insertedDish[0];
 
-    // Map ingredients to an array of objects for insertion
+    // Inserindo os ingredients passado no dish na tabela de ingredients
     const ingredientsInsert = ingredients.map(ingredient => ({
       title: ingredient,
       dish_id,
@@ -29,6 +29,25 @@ class DishesController {
 
     // Return a success response
     return response.status(201).json();
+  }
+
+  async show(request, response) {
+    // pegando o id do prato que é passado como parâmetro
+    const { id } = request.params;
+
+    // Fazendo a busca na tabela dishes e pedindo para buscar somente o dado que tenha o ID que foi passado como parâmetro.
+    const dish = await knex('dishes').where({ id }).first();
+
+    // Fazendo a busca dos ingredientes também. Passando a dish_id que é o parâmetro passado pela request e ordenado por ordem alfabética
+    const ingredients = await knex('ingredients')
+      .where({ dish_id: id })
+      .orderBy('title');
+
+    // Fazendo o retorno
+    return response.status(200).json({
+      ...dish,
+      ingredients,
+    });
   }
 }
 
