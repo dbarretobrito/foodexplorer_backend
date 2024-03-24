403f1f02bb222bf199afc1f69dadf4780f151002
daniel <dbarretobrito@gmail.com>
2024-03-24 16:45:27 -0300

    Cria um MIDDLEWARE de autenticação

---
+++ src/middleware/ensureAuthenticated.js
@@ -0,0 +1,31 @@
+// Importações referentes ao JWT
+const { verify } = require('jsonwebtoken');
+const authJwtConfig = require('../configs/auth');
+
+const AppError = require('../utils/AppError');
+
+function ensureAuthenticated(request, response, next) {
+  const authHeader = request.headers.authorization;
+
+  // Verificação a existência do token:
+  if (!authHeader) {
+    throw new AppError('JWT Token não informado', 401);
+  }
+
+  // Caso o token exista, será colocado na posição 1 do array:
+  const [, token] = authHeader.split(' ');
+
+  // Verificação se o token é válido
+  try {
+    const { sub: user_id } = verify(token, authJwtConfig.jwt.secret);
+    request.user = {
+      id: Number(user_id),
+    };
+
+    return next();
+  } catch {
+    throw new AppError('JWT Token inválido', 401);
+  }
+}
+
+module.exports = ensureAuthenticated;
