229005d0a12a85b20583d20146b3bde0a2d711e3
daniel <dbarretobrito@gmail.com>
2024-03-24 20:19:42 -0300

    Cria o método de UPDATE no UserController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs');
+const { hash, compare } = require('bcryptjs');
 
 const AppError = require('../utils/AppError');
 
@@ -33,6 +33,76 @@ class UsersController {
     );
 
     return response.status(201).json();
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id]);
+
+    // Verificação se o usuário existe
+    if (!user) {
+      throw new AppError('Usuário não encontrado');
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    );
+
+    // Verificando se ele encontrou um email e se email já está em uso
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('Este e-mail já está em uso!');
+    }
+
+    // Atribuindo novos valores para os campos no banco de dados, caso exista. Caso não exista, será mantido o valor do banco de dados
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    // Verificação da senha
+    // SE for informada a nova senha, porém, não for informado a senha antiga:
+    if (password && !old_password) {
+      throw new AppError(
+        'Para redefinir a senha é necessário informar a antiga'
+      );
+    }
+
+    // SE a nova senha e a antiga senha forem informadas:
+    if (password && old_password) {
+      // Verificando se a old_password é REALMENTE a senha que consta no db
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (checkOldPassword) {
+        if (password === old_password) {
+          throw new AppError(
+            'A nova senha informada é exatamente igual à anterior'
+          );
+        }
+      } else if (!checkOldPassword) {
+        // Se a senha informada não bater com a senha que está no db
+        throw new AppError('A senha antiga não confere');
+      }
+
+      // Atualizando a senha com criptografia
+      user.password = await hash(password, 8);
+    }
+
+    // Atualizando os valores
+    await database.run(
+      `
+        UPDATE users SET
+        name = ?,
+        email = ?,
+        password = ?,
+        updated_at = DATETIME('now')
+        WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+    );
+
+    return response.status(200).json();
   }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,13 +5,17 @@ const { Router } = require('express');
 const UsersController = require('../controllers/UsersController');
 const usersController = new UsersController();
 
+// Importando Middleware de autenticação
+const ensureAuthenticated = require('../middleware/ensureAuthenticated');
+
 // Inicializando
 const usersRoutes = Router();
 
 // Rotas
 // Não é preciso usar mais o '/users' só a '/' já funciona
 // Não é preciso passar (request, response) só o método que tem dentro da controller
-usersRoutes.post('/', usersController.create)
+usersRoutes.post('/', usersController.create);
+usersRoutes.put('/:id', usersController.update);
 
 // Exportando
 module.exports = usersRoutes;
